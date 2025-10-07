b4baafc30d0be2c25ef758ef959716b19b998223
AsmanRudi <asmanrudi277@gmail.com>
2025-10-07 20:20:00 +0700

    perubahaan

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
+import Home from './pages/home';
+import { About } from './pages/about';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+  <Home></Home>
+  <About></About>
   </React.StrictMode>
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/pages/home.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+class Home extends React.Component{
+    constructor(){
+        super()
+    }
+
+    render(){
+        return(
+            <h1> Ini Halaman Home dengan Class Component</h1>
+        )
+    }
+}
+
+export default Home
